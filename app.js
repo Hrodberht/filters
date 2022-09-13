ab9b61a5eee4a7fe93ac3476c1071cd39fbf2ef8
Hrodberht <hrodberht.stefanescu@gmail.com>
2022-09-13 09:19:33 +0100

    Added no results condition

--- app.js
+++ app.js
@@ -3,6 +3,11 @@ let filteredProducts = [...products];
 const productsContainer = document.querySelector(".products-container");
 
 const displayProducts = () => {
+  if (filteredProducts.length < 1) {
+    productsContainer.innerHTML = `<h6>Sorry, no products matched your search</h6>`;
+    return;
+  }
+
   productsContainer.innerHTML = filteredProducts
     .map(({ id, title, image, price }) => {
       return `<article class="product" data-id=${id}>
